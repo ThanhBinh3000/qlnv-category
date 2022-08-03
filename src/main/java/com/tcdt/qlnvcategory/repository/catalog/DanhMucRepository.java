080c1d4f60a11c7c222de4b5c1f68d2a63ad66b2
thongtv <thongtv@tecapro.com>
2022-08-03 15:12:42 +0700

    fix dm dung chung

--- src/main/java/com/tcdt/qlnvcategory/repository/catalog/DanhMucRepository.java
+++ src/main/java/com/tcdt/qlnvcategory/repository/catalog/DanhMucRepository.java
@@ -2,23 +2,33 @@ package com.tcdt.qlnvcategory.repository.catalog;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.tcdt.qlnvcategory.table.catalog.QlnvDanhMuc;
 
+import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 public interface DanhMucRepository extends CrudRepository<QlnvDanhMuc, Long> {
 
-    @Query(value = "SELECT * FROM DM_DUNG_CHUNG dm WHERE (:ma is null or lower(dm.MA) like lower(concat(concat('%', " +
-            ":ma),'%'))) AND   (:maCha is null or lower(dm.MA_CHA) like lower(concat(concat('%', " + ":maCha),'%'))) " +
-            "AND " + "(:trangThai is null or lower(dm.TRANG_THAI) = :trangThai) AND (:giaTri is null or lower(dm" +
-            ".GIA_TRI) = " +
-            ":giaTri) AND (:loai is null or lower(dm.LOAI) = :loai)",
-            nativeQuery =
-                    true)
-    Page<QlnvDanhMuc> selectParams(String ma, String maCha, String trangThai, String giaTri, String loai, Pageable pageable);
+    @Query(value = "SELECT * FROM DM_DUNG_CHUNG dm " +
+            "WHERE (:ma IS NULL OR LOWER(dm.MA) LIKE LOWER(CONCAT(CONCAT('%',:ma),'%'))) " +
+            "AND   (:maCha IS NULL OR LOWER(dm.MA_CHA) LIKE LOWER(CONCAT(CONCAT('%',:maCha),'%'))) " +
+            "AND   (:trangThai IS NULL OR LOWER(dm.TRANG_THAI) = :trangThai)" +
+            "AND   (:giaTri IS NULL OR LOWER(dm.GIA_TRI) = :giaTri) " +
+            "AND   (:loai IS NULL OR LOWER(dm.LOAI) = :loai)",
+            nativeQuery = true)
+    Page<QlnvDanhMuc> selectPage(String ma, String maCha, String trangThai, String giaTri, String loai, Pageable pageable);
+//    @Query(value ="SELECT * FROM DM_DUNG_CHUNG DM WHERE (:ma IS NULL OR LOWER(DM.MA)=:ma )"
+//            +"AND (:maCha IS NULL OR LOWER(DM.MA_CHA) =:maCha )"
+//            +"AND (:trangThai IS NULL OR LOWER(DM.TRANG_THAI)  = :trangThai )"
+//            +"AND (:giaTri IS NULL OR LOWER(DM.GIA_TRI) =:giaTri )"
+//            +"AND (:loai IS NULL OR LOWER(DM.LOAI) =:loai )"
+//            , nativeQuery = true)
+//    Page<QlnvDanhMuc> selectPage(String ma, String maCha, String trangThai, String giaTri, String loai, Pageable pageable);
 
 
     Iterable<QlnvDanhMuc> findByTrangThai(String trangThai);
@@ -28,6 +38,10 @@ public interface DanhMucRepository extends CrudRepository<QlnvDanhMuc, Long> {
     QlnvDanhMuc findByMa(String ma);
 
     Optional<QlnvDanhMuc> findById(Long id);
+
+    @Transactional
+    @Modifying
+    void deleteAllByIdIn(List<Long> ids);
 
 
 }
