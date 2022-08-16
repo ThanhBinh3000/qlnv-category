204ca64fa2213880fd8e60e482a345f766382347
VuNT <vunt@ttkd6.com>
2022-08-16 13:44:13 +0700

    fix danh muc dung chung

--- src/main/java/com/tcdt/qlnvcategory/repository/catalog/DanhMucRepository.java
+++ src/main/java/com/tcdt/qlnvcategory/repository/catalog/DanhMucRepository.java
@@ -11,12 +11,16 @@ import java.util.Optional;
 
 public interface DanhMucRepository extends CrudRepository<QlnvDanhMuc, Long> {
 
-    @Query(value = "SELECT * FROM DM_DUNG_CHUNG dm " +
-            "WHERE (:ma IS NULL OR LOWER(dm.MA) LIKE LOWER(CONCAT(CONCAT('%',:ma),'%'))) " +
-            "AND   (:maCha IS NULL OR LOWER(dm.MA_CHA) LIKE LOWER(CONCAT(CONCAT('%',:maCha),'%'))) " +
-            "AND   (:trangThai IS NULL OR LOWER(dm.TRANG_THAI) = :trangThai)" +
-            "AND   (:giaTri IS NULL OR LOWER(dm.GIA_TRI) = :giaTri) " +
-            "AND   (:loai IS NULL OR LOWER(dm.LOAI) = :loai)",
+    @Query(value = "SELECT * FROM DM_DUNG_CHUNG DM " +
+            "WHERE (:ma IS NULL OR LOWER(DM.MA) LIKE LOWER(CONCAT(CONCAT('%',:ma),'%'))) " +
+            "AND   (:maCha IS NULL OR LOWER(DM.MA_CHA) LIKE LOWER(CONCAT(CONCAT('%',:maCha),'%'))) " +
+            "AND   (:trangThai IS NULL OR  LOWER ( " +
+            "    CASE WHEN DM.TRANG_THAI = '01' then 'Hoạt động' " +
+            "    WHEN DM.TRANG_THAI = '00' then 'Không hoạt động' " +
+            "    ELSE 'Không hoạt động' end" +
+            " ) LIKE LOWER(CONCAT(CONCAT('%',:trangThai),'%')))" +
+            "AND   (:giaTri IS NULL OR LOWER(DM.GIA_TRI) LIKE LOWER(CONCAT(CONCAT('%',:giaTri),'%'))) " +
+            "AND   (:loai IS NULL OR LOWER(DM.LOAI) LIKE LOWER(CONCAT(CONCAT('%',:loai),'%')))",
             nativeQuery = true)
     Page<QlnvDanhMuc> selectPage(String ma, String maCha, String trangThai, String giaTri, String loai, Pageable pageable);
     Iterable<QlnvDanhMuc> findByTrangThai(String trangThai);
